4b6456c7161d235f43bce7e7df76569fe974cb31
itachi5350 <11e34sarika@gmail.com>
2026-04-13 19:33:00 +0530

    Time: 4 ms (24.82%), Space: 115.6 MB (67.36%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
        int m1=INT_MAX, m2=INT_MAX;
-       for(const int n : nums){
+       for(auto n : nums){
         if(n<=m1) m1=n;
         else if(n<=m2) m2=n;
         else return true;
