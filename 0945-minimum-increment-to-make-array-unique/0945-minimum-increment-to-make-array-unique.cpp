3d2ea43b9cc17019b90cad7ce5e4bb4396522369
itachi5350 <11e34sarika@gmail.com>
2026-04-13 23:24:47 +0530

    Time: 36 ms (74.29%), Space: 69.4 MB (76.91%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -4,9 +4,10 @@ public:
         int c=0;
        sort(nums.begin(),nums.end());
        for(int i=1;i<nums.size();i++){
-          while(nums[i]<=nums[i-1]){
-             nums[i]+=1;
-             c++;
+          if(nums[i]<=nums[i-1]){
+             int n=(nums[i-1]+1)-nums[i];
+             c+=n;
+             nums[i]+=n;
        }
        }
        return c;
