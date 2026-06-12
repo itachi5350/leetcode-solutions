213e40a2ea6023a37b111f34e70799dd618fd891
itachi5350 <11e34sarika@gmail.com>
2026-06-12 18:43:06 +0530

    Time: 11 ms (51.64%), Space: 21 MB (33.9%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,12 +3,17 @@ public:
     int countSubstrings(string s) {
         int count =0;
         for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                string q=s.substr(i,j-i+1);
-                string p=q;
-                reverse(p.begin(),p.end());
-                if(p==q) count+=1;
-            }
+           count+=expand(s,i,i);
+           count+=expand(s,i,i+1);
+        }
+        return count;
+    }
+    int expand(string s, int l, int r){
+        int count=0;
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            count++;
+            l--;
+            r++;
         }
         return count;
     }
