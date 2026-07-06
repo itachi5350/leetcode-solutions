a0f0e226d8b9d780402d52c0d3f5c7382ae000e9
itachi5350 <11e34sarika@gmail.com>
2026-07-06 21:00:38 +0530

    Time: 0 ms (100%), Space: 17.5 MB (89.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& v, int k) {
-        int l=-1,r=-1;
-       for(int i=0;i<v.size();i++){
-        if(v[i]==k) {
-            l=i;
-            int j=i;
-        while(j<v.size() && v[j]==k){
-              r=j;
-              j++;
-        }
-        break;
-        }
-       }
-       return {l,r};
+         int first=firstOccurrence(v,k);
+        if(first==-1) return {-1,-1};
+        int last=lastOccurrence(v,k);
+        return {first,last};
     }
+    int firstOccurrence(vector<int>& v, int k) {
+    int l = 0, r = v.size() - 1;
+    int ans = -1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (v[mid] == k) {
+            ans = mid;
+            r = mid - 1;
+        }
+        else if (v[mid] < k)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+
+    return ans;
+}
+int lastOccurrence(vector<int>& v, int k) {
+    int l = 0, r = v.size() - 1;
+    int ans = -1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (v[mid] == k) {
+            ans = mid;
+            l=mid+1;
+        }
+        else if (v[mid] < k)
+            l = mid + 1;
+        else
+            r = mid-1;
+    }
+
+    return ans;
+     }
 };
