2e8916a84686f05acadd5f2e60c5fd0015596bf2
itachi5350 <11e34sarika@gmail.com>
2026-07-06 20:20:32 +0530

    Time: 10 ms (46.8%), Space: 12.6 MB (55.7%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        unordered_map<int,int>f;
-        int l=0,res=0,mf=0;
-        for(int r=0;r<answerKey.size();r++){
-            f[answerKey[r]]++;
-            mf=max(mf,f[answerKey[r]]);
-            while((r-l+1)-mf>k){
-                f[answerKey[l]]--;
-                l++;
+        int l=0,res=0,s=answerKey.size(),cnt=0;
+        for(int r=0;r<s;r++){
+           if(answerKey[r]=='F') cnt++;
+           while(cnt>k){
+            if(answerKey[l]=='F'){
+                cnt--;
             }
-            res=max(res,r-l+1);
+            l++;
+           }
+        res=max(res,r-l+1);
         }
-        return res;
+        l=0,cnt=0;
+        for(int r=0;r<s;r++){
+           if(answerKey[r]=='T') cnt++;
+           while(cnt>k){
+            if(answerKey[l]=='T'){
+                cnt--;
+            }
+            l++;
+           }
+        res=max(res,r-l+1);
+        }
+       return res;
     }
 };
