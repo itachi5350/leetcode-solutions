369e7c4f68cc373df78a869882c9a0c768e57d34
itachi5350 <11e34sarika@gmail.com>
2026-07-07 23:53:32 +0530

    Time: 0 ms (100%), Space: 11.7 MB (36.43%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int>res;
-        if(p.size()>s.size()) return res;
-        vector<int>m(26,0);
-        vector<int>n(26,0);
-        for(int r=0; r<p.size();r++){
-            m[p[r]-'a']++;
-        }
+           vector<int>freq(26,0);
+        vector<int>f;
         int l=0;
-        for(int r=0; r<s.size();r++){
-            n[s[r]-'a']++;
-            if(r-l+1>p.size()){
-                n[s[l]-'a']--;
+        for(int i=0;i<p.size();i++){
+            freq[p[i]-'a']++;
+        }
+        int count = p.size();
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a']>0){
+                count--;
+            }
+            freq[s[i]-'a']--;
+            while(i-l+1  > p.size()){
+                if(freq[s[l]-'a']>=0)count++;
+                freq[s[l]-'a']++;
                 l++;
             }
-            if(r-l+1==p.size()){
-                if(issame(m,n)) res.push_back(l);
-            }
+            if(count==0)f.push_back(l);
+      
         }
-        return res;
-    }
-    bool issame(vector<int>& m, vector<int>& n){
-        for(int i=0;i<26;i++){
-            if(m[i]!=n[i]) return false;
-        }
-        return true;
+        return f ;
     }
 };
