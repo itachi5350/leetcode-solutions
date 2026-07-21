ced56a5d1ffc1c019b8935d481a2ad6511c79a0a
itachi5350 <11e34sarika@gmail.com>
2026-07-22 02:24:07 +0530

    Time: 2 ms (57.75%), Space: 7.9 MB (42.14%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,9 +5,9 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l=1,r=n;
-        while(l<=r){
+        while(l<r){
             int mid=l+(r-l)/2;
-            if(isBadVersion(mid)==true) r=mid-1;
+            if(isBadVersion(mid)==true) r=mid;
             else l=mid+1;
         }
         return l;
