062bdbce835a305ef65d2100abf28c4a6fd1b512
itachi5350 <11e34sarika@gmail.com>
2026-07-24 23:33:41 +0530

    Time: 0 ms (100%), Space: 23 MB (56.39%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -17,9 +17,8 @@ public:
     int dugu(TreeNode* root){
         if(!root)return 0;
         int l=dugu(root->left);
-        if(l==-1)return -1;
         int r=dugu(root->right);
-        if(r==-1)return -1;
+        if(l==-1 || r==-1)return -1;
         if(abs(l-r)>1) return -1;
         return 1 + max(l, r);   
     }
