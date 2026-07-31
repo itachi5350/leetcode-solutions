5339fd359a8676eb3b85b2f53fc1dfa5052ef805
itachi5350 <11e34sarika@gmail.com>
2026-07-31 06:22:48 +0530

    Time: 0 ms (100%), Space: 102 MB (85.6%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -7,11 +7,9 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    if(j>0 && grid[i][j-1]==0 || j==0) cnt++;
-                    if(i>0 && grid[i-1][j]==0|| i==0) 
-                    cnt++;
-                    if(j<m-1 && grid[i][j+1]==0 || j==m-1) cnt++;
-                    if(i<n-1 && grid[i+1][j]==0 || i==n-1) cnt++;
+                    cnt+=4;
+                    if(j+1<m && grid[i][j+1]==1) cnt-=2;
+                    if(i+1<n && grid[i+1][j]==1) cnt-=2;
                 }
             }
         }
