0349fa7330268ecceed55f66ee7c6e6fd24d2406
itachi5350 <11e34sarika@gmail.com>
2026-08-17 14:43:15 +0530

    Time: 4 ms (33.82%), Space: 23.1 MB (19.32%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -14,7 +14,7 @@ public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         return dugu(nums, 0,nums.size()-1);
     }
-    TreeNode* dugu(vector<int>& nums, int l, int r){
+    TreeNode* dugu(vector<int>&nums, int l, int r){
         if(l>r)return NULL;
         int mid=l+(r-l)/2;
         TreeNode* node=new TreeNode(nums[mid]);
