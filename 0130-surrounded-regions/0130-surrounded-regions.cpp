3b1eb070da2ab11dc44ae3207c35842c486e8f52
itachi5350 <11e34sarika@gmail.com>
2026-09-14 17:17:04 +0530

    Time: 0 ms (100%), Space: 13.8 MB (97.38%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O') dfs(board,i,0,m,n);
+        }
+        for(int i=0;i<m;i++){
+            if(board[i][n-1]=='O') dfs(board,i,n-1,m,n);
+        }
+         for(int j=0;j<n;j++){
+            if(board[0][j]=='O') dfs(board,0,j,m,n);
+         }
+          for(int j=0;j<n;j++){
+            if(board[m-1][j]=='O') dfs(board,m-1,j,m,n);
+          }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+                else if(board[i][j]=='1') board[i][j]='O';
+                }
+            }
+    }
+    void dfs(vector<vector<char>>& board,int i,int j,int m,int n){
+       if(i<0 or i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!='O') return;
+       board[i][j]='1';
+       dfs(board,i-1,j,m,n);
+       dfs(board,i+1,j,m,n);
+       dfs(board,i,j-1,m,n);
+       dfs(board,i,j+1,m,n);
+    }
+};
