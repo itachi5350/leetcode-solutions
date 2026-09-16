2f2d1a27f46b5e3c661d0ff2f826f5e5e11ba273
itachi5350 <11e34sarika@gmail.com>
2026-09-16 18:41:04 +0530

    Time: 4 ms (76.56%), Space: 22.2 MB (75.52%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>>path;
-         int m=heights.size();
+        int m=heights.size();
         int n=heights[0].size();
+        vector<vector<int>>res;
         vector<vector<bool>>paci(m,vector<bool>(n,false));
         vector<vector<bool>>atlan(m,vector<bool>(n,false));
-       
-        for(int j=0;j<n;j++){
-            dfs(0,j,m,n,heights,paci);
-        }
-         for(int j=0;j<n;j++){
-            dfs(m-1,j,m,n,heights,atlan);
-        }
-         for(int i=0;i<m;i++){
-            dfs(i,0,m,n,heights,paci);
-        }
-         for(int i=0;i<m;i++){
-            dfs(i,n-1,m,n,heights,atlan);
-        }
+        for(int j=0;j<n;j++) dfs(0,j,m,n,heights,paci);
+        for(int j=0;j<n;j++) dfs(m-1,j,m,n,heights,atlan);
+        for(int i=0;i<m;i++) dfs(i,0,m,n,heights,paci);
+        for(int i=0;i<m;i++) dfs(i,n-1,m,n,heights,atlan);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(paci[i][j] && atlan[i][j]) path.push_back({i,j});
+                if(paci[i][j] && atlan[i][j]) res.push_back({i,j});
             }
         }
-        return path;
+        return res;
     }
-    void dfs(int i,int j, int m, int n, vector<vector<int>>& heights,vector<vector<bool>>& v ){
-        if(i<0 || i>=m || j<0 || j>=n) return;
-        if(v[i][j]) return;
+    void dfs(int i, int j, int m, int n, vector<vector<int>> &heights,vector<vector<bool>> &v){
+        if(i<0 || i>=m || j<0 || j>=n || v[i][j]) return;
         v[i][j]=true;
-        if(i+1 < m && heights[i+1][j] >= heights[i][j])
-         dfs(i+1,j,m,n,heights,v);
-        if(i-1 >= 0 && heights[i-1][j] >= heights[i][j])
-         dfs(i-1,j,m,n,heights,v);
-        if(j+1 < n && heights[i][j+1] >= heights[i][j])
-         dfs(i,j+1,m,n,heights,v);
-        if(j-1 >= 0 && heights[i][j-1] >= heights[i][j])
-         dfs(i,j-1,m,n,heights,v);
+        if(i+1<m && heights[i][j]<=heights[i+1][j]) dfs(i+1,j,m,n,heights,v);
+        if(i-1>=0 && heights[i][j]<=heights[i-1][j]) dfs(i-1,j,m,n,heights,v);
+        if(j+1<n && heights[i][j]<=heights[i][j+1]) dfs(i,j+1,m,n,heights,v);
+        if(j-1>=0 && heights[i][j]<=heights[i][j-1]) dfs(i,j-1,m,n,heights,v);
     }
 };
